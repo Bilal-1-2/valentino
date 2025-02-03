ec642876fa8ce932b719fa3bdfb342605a984b6e
Bilal-1-2 <bilal127g14@gmail.com>
2025-02-03 13:41:33 +0100

    test

---
+++ sketch.js
@@ -0,0 +1,22 @@
+// Functie om de verrassing te tonen
+function showMessage() {
+    const message = document.getElementById('message');
+    const heart = document.getElementById('heart');
+    const music = document.getElementById('backgroundMusic');
+
+    // Toon het bericht
+    message.classList.remove('hidden');
+
+    // Speel achtergrondmuziek af
+    music.play();
+
+    // Verwijder de klikfunctie van het hartje
+    heart.style.pointerEvents = 'none';
+}
+
+// Voeg een geluidseffect toe bij het laden van de pagina
+window.onload = function () {
+    const audio = new Audio('click-sound.mp3');
+    audio.play();
+};
+//qfjydbcjasfu
